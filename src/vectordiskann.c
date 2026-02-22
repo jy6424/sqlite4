4ef774b3327960e6b1ce2916ae2d420b86153b64
jy6424 <yjw6424@naver.com>
2026-02-23 02:56:58 +0900

    test modified

--- src/vectordiskann.c
+++ src/vectordiskann.c
@@ -860,8 +860,10 @@ static int diskAnnInsertShadowRow(const DiskAnnIndex *pIndex, const VectorInRow 
     rc = SQLITE4_NOMEM;
     goto out;
   }
-  rc = sqlite4_prepare(pIndex->db, zSql, -1, &pStmt, 0);
+  rc = sqlite4_prepare(...);
   if( rc != SQLITE4_OK ){
+    printf("diskAnnInsertShadowRow prepare rc=%d errmsg=%s sql=%s\n",
+          rc, sqlite4_errmsg(pIndex->db), zSql);
     goto out;
   }
   for(i = 0; i < pVectorInRow->nKeys; i++){
