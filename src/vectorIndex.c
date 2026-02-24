dec99058fd23ae70c1e645ed79277c5d16b95d51
jy6424 <yjw6424@naver.com>
2026-02-24 16:41:44 +0900

    fixed

--- src/vectorIndex.c
+++ src/vectorIndex.c
@@ -1199,7 +1199,7 @@ int vectorIndexInsert(
     return rc;
   }
   printf("vectorIndexInsert: vectorInRowAlloc rc=%d, vectorInRow.pVector=%p, vectorInRow.nKeys=%d, vectorInRow.pKeyValues=%p\n", rc, vectorInRow.pVector, vectorInRow.nKeys, vectorInRow.pKeyValues);
-  printf("vectorIndexInsert: vectorInRow.pVector->type=%d, vectorInRow.pKeyValues->type\n", vectorInRow.pVector->type, vectorInRow.pKeyValues->type);
+  printf("vectorIndexInsert: vectorInRow.pVector->type=%d, vectorInRow.pKeyValues->type=%d\n", vectorInRow.pVector->type, vectorInRow.pKeyValues->type);
   if( vectorInRow.pVector == NULL ){
     return SQLITE4_OK;
   }
