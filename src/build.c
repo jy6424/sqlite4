a2b6fc1e9300f70c10512e5ac06d5c6d6e8fc465
jy6424 <yjw6424@naver.com>
2026-02-25 16:30:18 +0900

    added debug msg

--- src/build.c
+++ src/build.c
@@ -2986,7 +2986,7 @@ Index *sqlite4CreateIndex(
   ** step can be skipped.
   */
   else{
-    printf("createIndex: writing index %c\n", pName->z);
+    printf("createIndex: writing index %s\n", pName->z);
     createIndexWriteSchema(pParse, pIndex, pName, pEnd);
   }
 
